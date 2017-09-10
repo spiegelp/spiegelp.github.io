77f1af8c3910ba0ecb1c20782dba02ea965d6d13
Philipp Spiegel <spiegelp@gmx.at>
2017-09-10 12:28:04 +0200

    replace Material Design Lite by Material Components for the web

---
+++ js/init.js
@@ -0,0 +1,4 @@
+window.mdc.autoInit();
+
+let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
+document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
